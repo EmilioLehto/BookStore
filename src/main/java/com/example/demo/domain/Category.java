da5d9c3dba15565cfc39d8f60c3f8852a259de0a
EmilioLehto <emilio.lehto@gmail.com>
2024-04-04 08:48:46 +0300

    thymeleaf fixes

--- src/main/java/com/example/demo/domain/Category.java
+++ src/main/java/com/example/demo/domain/Category.java
@@ -13,50 +13,50 @@ import jakarta.persistence.OneToMany;
 
 @Entity
 public class Category {
-@Id
-@GeneratedValue(strategy = GenerationType.AUTO)
-private Long categoryId;
-private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long categoryId;
+    private String name;
 
-@JsonIgnore
-@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
-private List<Book> books;
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private List<Book> books;
 
-public Category(){}
+    public Category() {
+    }
 
-public Category(String name){
-    super();
-    this.name=name;
-}
+    public Category(String name) {
+        super();
+        this.name = name;
+    }
 
-public Long getCategoryId() {
-    return categoryId;
-}
+    public Long getCategoryId() {
+        return categoryId;
+    }
 
-public void setCategoryId(Long categoryId) {
-    this.categoryId = categoryId;
-}
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
 
-public String getName() {
-    return name;
-}
+    public String getName() {
+        return name;
+    }
 
-public void setName(String name) {
-    this.name = name;
-}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-public List<Book> getBooks() {
-    return books;
-}
+    public List<Book> getBooks() {
+        return books;
+    }
 
-public void setBooks(List<Book> books) {
-    this.books = books;
-}
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 
-@Override
-	public String toString() {
-		return "Category [categoryid=" + categoryId + ", name= " + name + "]";
-	}
-
+    @Override
+    public String toString() {
+        return "Category [categoryid=" + categoryId + ", name= " + name + "]";
+    }
 
 }
